d8bf27469bc4fcf5f7b2dee94310b8c49dc8aaaa
nigo <tujiabing81@163.com>
2021-03-14 01:13:20 +0800

    trayer

--- xmonad.hs
+++ xmonad.hs
@@ -181,6 +181,7 @@ myStartupHook = do
         spawnOnce "mailspring &"
         spawnOnce "mate-power-manager &"
         spawnOnce "albert &"
+        spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x282c34  --height 22 &"
 
 myScratchPads = [ NS "terminal" spawnTerm  findTerm manageTerm
 		, NS "music" spawnPav findPav  managePav
